317025611fea46bcce2ef31aef6cee1637c10cc1
Thanonchai-Paliwong <thanonchai.paliwong@fastfit.fun>
2023-01-10 16:55:14 +0700

    create carousel component

---
+++ src/components/carousel/index.tsx
@@ -0,0 +1,57 @@
+import Slider, { Settings, CustomArrowProps } from "react-slick";
+import styled from "./index.module.scss";
+import banner1 from "@/assets/banner/1.jpg";
+import banner2 from "@/assets/banner/2.jpg";
+import banner3 from "@/assets/banner/3.png";
+import banner4 from "@/assets/banner/4.jpg";
+import banner5 from "@/assets/banner/5.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+type Props = {};
+
+function Carousel({}: Props) {
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: styled.dot,
+    appendDots: (dots) => <ul>{dots}</ul>,
+    customPaging: () => <span />,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <div className={styled.carousel}>
+      <Slider {...settings}>
+        <img src={banner1} alt="banner" />
+        <img src={banner2} alt="banner" />
+        <img src={banner3} alt="banner" />
+        <img src={banner4} alt="banner" />
+        <img src={banner5} alt="banner" />
+      </Slider>
+    </div>
+  );
+}
+
+export default Carousel;
+
+function SampleNextArrow(props: CustomArrowProps) {
+  const { onClick } = props;
+  return (
+    <span className={`${styled["arrow-container"]} ${styled["next-arrow"]}`} onClick={onClick}>
+      <i className="fa-solid fa-angle-right" />
+    </span>
+  );
+}
+
+function SamplePrevArrow(props: CustomArrowProps) {
+  const { onClick } = props;
+  return (
+    <span className={`${styled["arrow-container"]} ${styled["prev-arrow"]}`} onClick={onClick}>
+      <i className="fa-solid fa-angle-left" />
+    </span>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,14 @@
+import Carousel from "../../components/carousel";
+import styled from "./index.module.scss";
+
 type Props = {};
 
 function index({}: Props) {
-  return <div>Home Page</div>;
+  return (
+    <div className={styled.home}>
+      <Carousel />
+    </div>
+  );
 }
 
 export default index;
