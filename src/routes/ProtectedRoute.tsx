38dc1edb8015d7d8e2fc59a9915d3781e0ec15e2
Thanonchai Paliwong <t.paliwong@gmail.com>
2023-02-09 21:00:04 +0700

    Create Protected Route

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { toast } from "react-toastify";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useAppSelector } from "store/configureStore";
+
+const ProtectedRoute = () => {
+  const { user } = useAppSelector((state) => state.account);
+  const location = useLocation();
+
+  if (!user) {
+    toast.error("You need to be logged in to do that!");
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return <Outlet />;
+};
+
+export default ProtectedRoute;
