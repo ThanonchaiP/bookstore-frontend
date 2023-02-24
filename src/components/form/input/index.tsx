72465254459fa570ad85ce1de5613e64937d560a
Thanonchai Paliwong <t.paliwong@gmail.com>
2023-02-24 20:06:14 +0700

    Create Hide/Show Password

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, forwardRef } from "react";
+import { ComponentProps, forwardRef, useState } from "react";
 import classnames from "classnames";
 import styles from "./index.module.scss";
 
@@ -15,6 +15,9 @@ export const FormInput = forwardRef<HTMLInputElement, InputProps>(function Input
   { label, type = "text", className = "", error, required, containerClassName = "", labelClassName, ...props },
   ref
 ) {
+  const [inputType, setInputType] = useState(type);
+  const onChangeType = () => setInputType((state) => (state === "text" ? "password" : "text"));
+
   return (
     <div className={classnames([styles["form-group"], containerClassName])}>
       <label className={classnames("inline-block mb-2", labelClassName)}>
@@ -22,12 +25,23 @@ export const FormInput = forwardRef<HTMLInputElement, InputProps>(function Input
         {required && <span className="text-red-500 ml-1">*</span>}
       </label>
 
-      <input
-        className={classnames([styles.input, className], { [styles["input-invalid"]]: error })}
-        type={type}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          className={classnames([styles.input, className], { [styles["input-invalid"]]: error })}
+          type={inputType}
+          ref={ref}
+          {...props}
+        />
+
+        {type === "password" && (
+          <i
+            className={`absolute top-[6px] right-3 text-xl cursor-pointer text-[#595959] fa-regular ${
+              inputType === "password" ? "fa-eye-slash" : "fa-eye"
+            } `}
+            onClick={onChangeType}
+          />
+        )}
+      </div>
       {error && <span className="inline-block mt-1 text-red-500">{error}</span>}
     </div>
   );
